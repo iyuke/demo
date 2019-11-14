87d20d8271b0d37a93628bf37a660ba526aeda42
yuke <yuke@youzan.com>
2019-11-14 20:47:13 +0800

    some commit

---
+++ src/main/java/com/example/jenkins/demo/web/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.jenkins.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author yuke
+ * @Date 2019-11-14 20:42
+ */
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+}
